10deaf0e7285040008cbdab64fc1842919656a6e
Sachini Usha <sachini.usha123@gmail.com>
2023-04-19 04:08:41 +0530

    modified student profile

--- src/main/webapp/components2/featured_cont.js
+++ src/main/webapp/components2/featured_cont.js
@@ -40,7 +40,7 @@ class Featured_cont extends HTMLElement {
         for (let element of addtocart){
             element.addEventListener('click',async (event)=>{
 
-                let content_id = event.target.parentElement.id;
+                let content_id = event.target.id;
 
 
                 let requestBody= {
@@ -51,14 +51,15 @@ class Featured_cont extends HTMLElement {
                     response.json()
 
                 );
-
-                if(res.message==="You already added this content"){
-                    alert(res.message);
-                }
-
-                else{
-                    alert("added content successfully");
-                }
+                // alert(res.message);
+                //
+                // if(res.message==="You already added this content"){
+                //     alert(res.message);
+                // }
+                //
+                // else{
+                //     alert("added content successfully");
+                // }
 
                 // let location = window.location;
                 // window.history.pushState({}, "", location);
@@ -92,5 +93,5 @@ class Featured_cont extends HTMLElement {
 
     }
 }
-    
+
 customElements.define('featured-cont', Featured_cont);
