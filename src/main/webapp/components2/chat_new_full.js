9c5dc231455ca4d0c7047d64cc56ea0360362757
senithkay <senithkarunarathneu@gmail.com>
2023-05-07 03:27:03 +0530

    completed chat floating icon live chats

--- src/main/webapp/components2/chat_new_full.js
+++ src/main/webapp/components2/chat_new_full.js
@@ -7,7 +7,8 @@ class Chat_new_full extends HTMLElement {
         let title= this.attributes.title.value;
         let description= this.attributes.description.value;
         let author = this.attributes.author.value;
-        // let authour_ID =
+        let author_id = this.attributes.author_id.value
+
 
 
 
@@ -27,7 +28,7 @@ class Chat_new_full extends HTMLElement {
                     placeholder="Type your response">
 
                   <span class="material-icons">attach_file</span>
-                   <button  class="send-button js-quession-chat-send-btn${chat_id}"  type="submit"><span class="material-icons">send</span></button>
+                   <button  class="send-button js-quession-chat-send-btn${chat_id}"><span class="material-icons">send</span></button>
                 </form>
               </div>
             </div>
@@ -51,52 +52,36 @@ class Chat_new_full extends HTMLElement {
 
         `;
 
-
         let messageSendButton = document.querySelector(`.js-quession-chat-send-btn${chat_id}`);
         let message = document.querySelector(`.js-quession-chat-input${chat_id}`);
-        let chatContainer = document.querySelector(`#open-question-chat${chat_id}`);
-
-        messageSendButton.addEventListener('click', (event)=>{
-            event.preventDefault();
-            alert(`weda bn ithn${chat_id}`)
-            let newMessage = document.createElement('chat-msg');
-            newMessage.setAttribute("msg", "AWA SAGOR");
-            newMessage.setAttribute("type", "open-question-outgoingMSG");
-            chatContainer.appendChild(newMessage);
-
-        })
-
-
         let messageData = {};
-
         messageSendButton.addEventListener('click', (event)=>{
-            alert("send btn ek wed")
+
             event.preventDefault();
+            alert("send btn ek wed2")
             if (message.value != ""){
                 messageData = {
                     "config" : false,
                     "message" : message.value,
-                    "receiver": parseInt(author_ID),
+                    "receiver": parseInt(author_id),
                     "sender" : getUserID(),
-                    "questionId" : parseInt(questionid)
+                    "questionId" : parseInt(chat_id)
                 }
                 console.log(messageData)
 
                 questionCHat.send(JSON.stringify(messageData));
                 let chatContainer = document.querySelector(".open-question-chat");
                 if (chatContainer){
-                    let newMessage = document.createElement('chat-msg');
-                    newMessage.setAttribute("msg", "AWA SAGOR");
-                    newMessage.setAttribute("type", "open-question-outgoingMSG");
-                    chatContainer.appendChild(newMessage);
+                    chatContainer.innerHTML += `
+                            <div class="open-question-msg open-question-outgoingMSG"> <p>${message.value}</p></div>
+                            <br>
+                        `
+                    alert("methnt awne")
                     chatContainer.scrollTop = chatContainer.scrollHeight;
                 }
             }
             message.value = "";
         })
-
-
-
 
     }
 }
