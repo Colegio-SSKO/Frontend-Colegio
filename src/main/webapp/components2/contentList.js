965d9ad2708253bb8ba2cf209c32940e72bc9719
senithkay <senithkarunarathneu@gmail.com>
2023-05-08 18:36:26 +0530

    bug fix round 2

--- src/main/webapp/components2/contentList.js
+++ src/main/webapp/components2/contentList.js
@@ -9,21 +9,53 @@ class ContentList extends HTMLElement {
 
         let htmlcontent = "";
 
-        for (let i of data) {
-            htmlcontent += `<small-listed content_id="${i["content_id"]}" img_src="${i["img_src"]}" title="${i["title"]}" author="${i["author"]}" price="${i["price"]}" rates="${i["rate_count"]}"></small-listed>
-            `;
+        let count = 4
+
+        for (let i = 0; i<count && i < data.length ; i++){
+            htmlcontent += `<small-listed content_id="${data[i]["content_id"]}" img_src="${data[i]["img_src"]}" title="${data[i]["title"]}" author="${data[i]["author"]}" price="${data[i]["price"]}" rates="${data[i]["rate_count"]}"></small-listed>
+                    `;
         }
 
+        // for (let i of data) {
+        //     htmlcontent += `<small-listed content_id="${i["content_id"]}" img_src="${i["img_src"]}" title="${i["title"]}" author="${i["author"]}" price="${i["price"]}" rates="${i["rate_count"]}"></small-listed>
+        //     `;
+        // }
+
         this.innerHTML = `
-         <div class="content-list-wrap fadeInanimation">
+         <div id="js-home-cont-container" class="content-list-wrap fadeInanimation">
             
                  `+ htmlcontent +`
             </div>
-            <div class="more-btn"><button class="fnt-bold fnt fnt-mid btn btn-special more-btn">Show more</button></div>
+            <div id="js-show-more-cont-list" class="more-btn"><button class="fnt-bold fnt fnt-mid btn btn-special more-btn">Show more</button></div>
             
         `;
+        let showMore = document.querySelector('#js-show-more-cont-list');
+        let contContainer = document.querySelector('#js-home-cont-container')
+        let htmlPart = "";
+
+        showMore.addEventListener('click', (event)=>{
+            event.preventDefault();
+            htmlPart = "";
+            count += 4;
+            if (count>= data.length){
+                showMore.style.display = "none";
+            }
+            for (let i = 0; i<count && i < data.length ; i++){
+                htmlPart += `<small-listed content_id="${data[i]["content_id"]}" img_src="${data[i]["img_src"]}" title="${data[i]["title"]}" author="${data[i]["author"]}" price="${data[i]["price"]}" rates="${data[i]["rate_count"]}"></small-listed>
+                    `;
+
+
+            }
+           
+            contContainer.innerHTML = htmlPart;
+
+
+        })
+
     }
 }
+
+
 
 customElements.define('content-list', ContentList);
 
